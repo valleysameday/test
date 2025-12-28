7539c6a2c1116107fe6a061b9a18efb79cab08b1
valleysameday <valleysameday@gmail.com>
2025-12-28 16:14:42 +0000

    Update home.js

--- views/home.js
+++ views/home.js
@@ -2,4 +2,4 @@ import { initFeed } from "/index/js/feed.js";
 import { initUIRouter } from "/index/js/ui.js";
 
 initFeed();       // ✅ Weather, categories, posts
-initUIRouter();   // ✅ Modals, action bar, category buttons
+initUI();   // ✅ Modals, action bar, category buttons
