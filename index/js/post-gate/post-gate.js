388bd171a5cf0d44ff7c07425eb8a42ee7c5feac
valleysameday <valleysameday@gmail.com>
2026-01-13 22:16:33 +0000

    Update post-gate.js

--- index/js/post-gate/post-gate.js
+++ index/js/post-gate/post-gate.js
@@ -1,6 +1,7 @@
 import { initPostFlow } from "./post-flow.js";
 import { initPostMedia } from "./post-media.js";
 import { initPostSubmit } from "./post-submit.js";
+import { initTemplateLoader } from "./post-templates.js";
 
 let started = false;
 
@@ -8,6 +9,7 @@ export function initPostGate() {
   if (started) return;
   started = true;
 
+  initTemplateLoader();   // ⭐ NEW
   initPostFlow();
   initPostMedia();
   initPostSubmit();
