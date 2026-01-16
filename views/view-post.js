97259735a6fea8fd783fd27193634d85cbd5b8b4
valleysameday <valleysameday@gmail.com>
2026-01-16 15:18:19 +0000

    Update view-post.js

--- views/view-post.js
+++ views/view-post.js
@@ -240,7 +240,7 @@ followBtn.onclick = async () => {
   }
 
   const uid = window.currentUser.uid;
-  const ref = doc(db, "users", uid, "followers", post.userId);
+  const ref = doc(db, "users", post.userId, "followers", uid);
   const snap = await getDoc(ref);
 
   if (snap.exists()) {
