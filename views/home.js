76f1c0faecae1f098d839757f5509184db597a13
valleysameday <valleysameday@gmail.com>
2026-01-16 00:04:34 +0000

    Update home.js

--- views/home.js
+++ views/home.js
@@ -2,10 +2,12 @@ console.log("🏠 home.js loaded");
 
 import { initFeed } from "/index/js/feed.js";
 import { initUI } from "/index/js/ui.js";
+import { initWeather } from "/index/js/weather.js";
 
 export function init() {
   console.log("🏠 home.init() called");
 
   initFeed();   // 🔁 MUST run every time home loads
+initWeather();
   initUI();     // 🔁 safe to re-run
 }
