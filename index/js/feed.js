d176751abf00cb96a1c86765b7cc443346a14bde
valleysameday <valleysameday@gmail.com>
2026-01-19 20:56:39 +0000

    Update feed.js

--- index/js/feed.js
+++ index/js/feed.js
@@ -244,6 +244,9 @@ function buildPostCard(post, category) {
      BADGE OVERLAY
   ------------------------------ */
   let badgeHtml = "";
+  if (post.isBoosted && post.boostEnd > Date.now()) {
+  badgeHtml = `<div class="badge-overlay boosted">Boosted</div>`;
+  }
   if (post.featured) badgeHtml = `<div class="badge-overlay featured">Featured</div>`;
   else if (post.spotlight) badgeHtml = `<div class="badge-overlay spotlight">Spotlight</div>`;
   else if (post.urgent) badgeHtml = `<div class="badge-overlay urgent">Urgent</div>`;
