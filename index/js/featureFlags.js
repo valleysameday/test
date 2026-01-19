0889f300158cf432ed1693b83e2a9ac507d5d1b0
valleysameday <valleysameday@gmail.com>
2026-01-19 23:29:39 +0000

    Update featureFlags.js

--- index/js/featureFlags.js
+++ index/js/featureFlags.js
@@ -1,6 +1,6 @@
 export const featureFlags = {
-  boostingEnabled: true, // false, 
+  boostingEnabled: false, // false, 
   paymentsEnabled: true, //false,
-  stripeLiveMode: true //false
+  stripeLiveMode: false //false
 };
 window.STRIPE_PUBLISHABLE_KEY_LIVE = "pk_live_xxxxxxxxxxxxx";
