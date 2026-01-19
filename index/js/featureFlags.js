ecd0cc1f89e4857c8b69e24b045c1380ec5ea4d8
valleysameday <valleysameday@gmail.com>
2026-01-19 05:42:27 +0000

    Add Stripe publishable key to feature flags

--- index/js/featureFlags.js
+++ index/js/featureFlags.js
@@ -2,4 +2,5 @@ export const featureFlags = {
   boostingEnabled: true, // false, 
   paymentsEnabled: true, //false,
   stripeLiveMode: true //false
+  window.STRIPE_PUBLISHABLE_KEY_LIVE = "pk_live_xxxxxxxxxxxxx";
 };
