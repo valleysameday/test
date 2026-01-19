1ff9d2ad6be5101572c703c826d207f7b290d669
valleysameday <valleysameday@gmail.com>
2026-01-19 05:45:27 +0000

    Fix featureFlags.js to include STRIPE_PUBLISHABLE_KEY_LIVE

--- index/js/featureFlags.js
+++ index/js/featureFlags.js
@@ -2,5 +2,5 @@ export const featureFlags = {
   boostingEnabled: true, // false, 
   paymentsEnabled: true, //false,
   stripeLiveMode: true //false
-  window.STRIPE_PUBLISHABLE_KEY_LIVE = "pk_live_xxxxxxxxxxxxx";
 };
+window.STRIPE_PUBLISHABLE_KEY_LIVE = "pk_live_xxxxxxxxxxxxx";
