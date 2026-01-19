8dff24472dff8c20e872934703e35b25ffb7b7c4
valleysameday <valleysameday@gmail.com>
2026-01-19 00:56:26 +0000

    Create galleryAds.js

---
+++ postViews/galleryAds.js
@@ -0,0 +1,14 @@
+// /postViews/galleryAds.js
+
+export const galleryAds = [
+  {
+    src: "/ads/rctx-ad-1.jpg",
+    link: "https://rctx.wales/upgrade",
+    label: "Sponsored"
+  },
+  {
+    src: "/ads/rctx-ad-2.jpg",
+    link: "https://rctx.wales/business",
+    label: "Promoted"
+  }
+];
